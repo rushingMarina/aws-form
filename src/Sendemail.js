cd88fb1441a0fba344c4f92c520e516b8252fb63
mordva42 <333marina2007@gmail.com>
2020-06-11 14:59:19 -0700

    emails are now sending !!

--- src/Sendemail.js
+++ src/Sendemail.js
@@ -1,13 +1,20 @@
 
+  export default function sendEmail (toEmails, subject, message) {
+    new Date();
+    let current_datetime = new Date();
+    toEmails = [toEmails];
 
-export default function sendEmail (toEmails, subject, message) {
-  return fetch('https://f2jkjy36k8.execute-api.us-west-2.amazonaws.com/Prod/send', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      "Accept": "*/*",
-      "Host": "f2jkjy36k8.execute-api.us-west-2.amazonaws.com",
-    },
-    body: JSON.stringify({ toEmails, subject, message })
-  })
-}
+    console.log(current_datetime.toString());
+    return fetch('https://f2jkjy36k8.execute-api.us-west-2.amazonaws.com/Prod/send', {
+      method: 'POST',
+          headers: {
+            "Accept": "*/*",
+            "Host": "f2jkjy36k8.execute-api.us-west-2.amazonaws.com",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Origin": "http://mkdecision-form.s3-website-us-west-2.amazonaws.com",
+
+            "Date": JSON.stringify({ current_datetime })
+          },
+      body: JSON.stringify({ toEmails, subject, message })
+    })
+  }
