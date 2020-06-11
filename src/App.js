45ba9e392d05271ca193c20baa1ac2d39c7f41d0
mordva42 <333marina2007@gmail.com>
2020-06-10 17:30:15 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Simpleform from './Simpleform'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const initialState = {
+  user: {
+    name: '',
+    email: '',
+    message: ''
+  }
+}
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = initialState;
+    this.callbackFunction = this.callbackFunction.bind(this);
+  }
+
+  callbackFunction = (childData) => {
+      this.setState({user: childData})
+}
+
+  render() {
+    return (
+      <div className="App">
+
+        <Simpleform parentCallback = {this.callbackFunction}/>
+        <p> {this.state.user.name} </p>
+        <p> {this.state.user.email} </p>
+        <p> {this.state.user.message} </p>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Simpleform.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+
+export default class SimpleFormExample extends React.Component {
+    state = {
+        formData: {
+            name: '',
+            email: '',
+            message: ''
+        },
+        submitted: false,
+    }
+
+    handleChange = (event) => {
+        const { formData } = this.state;
+        formData[event.target.name] = event.target.value;
+        this.setState({ formData });
+    }
+
+    handleSubmit = () => {
+        this.setState({ submitted: true }, () => {
+            setTimeout(() => this.setState({ submitted: false }), 5000);
+            //sending data to the App parent
+            var user1 = this.state.formData;
+            this.props.parentCallback(user1);
+        });
+
+    }
+
+    render() {
+        const { formData, submitted } = this.state;
+        return (
+            <ValidatorForm
+                ref="form"
+                onSubmit={this.handleSubmit}
+            >
+                <h2>Sample form</h2>
+                <TextValidator
+                    label="Name"
+                    onChange={this.handleChange}
+                    name="name"
+                    value={formData.name}
+                    validators={['required']}
+                    errorMessages={['this field is required']}
+                />
+                <br/>
+                <br/>
+                <TextValidator
+                    label="Email"
+                    onChange={this.handleChange}
+                    name="email"
+                    value={formData.email}
+                    validators={['required', 'isEmail']}
+                    errorMessages={['this field is required', 'email is not valid']}
+                />
+                <br />
+                <br />
+                <br />
+                <TextValidator
+                    id="outlined-multiline-static"
+                    label="Your Message"
+                    onChange={this.handleChange}
+                    name="message"
+                    multiline
+                    rows={4}
+                    value = {formData.message}
+                    variant="outlined"
+                    validators={['required']}
+                    errorMessages={['this field is required']}
+                />
+                <br />
+                <br />
+                <Button
+                    color="primary"
+                    variant="contained"
+                    type="submit"
+                    disabled={submitted}
+                >
+                    {
+                        (submitted && 'Submitted!')
+                        || (!submitted && 'Submit')
+                    }
+                </Button>
+            </ValidatorForm>
+        );
+    }
+}
